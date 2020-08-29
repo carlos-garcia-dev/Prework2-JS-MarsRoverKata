38f64ef2aebbda76c3396a3f257441df2159df77
carlos-garcia-dev <contacto.dvlpr@protonmail.com>
2020-08-29 21:45:58 +0200

    0.0-initialSetUp

---
+++ script.js
@@ -0,0 +1,27 @@
+/* Carlos García - LAB | JS: Mars Rover Kata
+https://github.com/carlos-garcia-dev
+https://www.codewars.com/users/cdvlpr 
+Madrid - Octubre 2020 */
+
+const rover = { 
+    x: 0,
+    y: 0,
+    direction: 0
+}
+
+
+function turnLeft(rover){
+    console.log('turnLeft was called')    
+}
+
+function turnRight(rover){
+    console.log('turnRight was called')    
+}
+
+function goForward(rover){
+    console.log('goForward was called')    
+}
+
+function commands(rover){
+    console.log('commands')
+}
